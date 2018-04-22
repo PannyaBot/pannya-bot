7450c8b1d9b91a864ccab85470560a58a9bf152d
PannyaBot <pannyabot@tutanota.com>
2018-04-21 23:22:31 -0700

    doh

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ var Discord = require('discord.js');
 var client = new Discord.Client();
 var auth = rquire('auth.json');
 
-client.on("!help", function(message) {
+client.on("message", function(message) {
     if (message.content === "ping") {
         message.reply('nothing');
     }
 });
 
-client.on("!help", function(message) {
-    if (message.content === "ping") {
-        message.reply('nothing');
-    }
-});
+// client.on("message", function(message) {
+//     if (message.content === "ping") {
+//         message.reply('nothing');
+//     }
+// });
 
 client.login(auth.token);
