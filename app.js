925da4b9c07bdd474161853b63f4ec85440f94e9
PannyaBot <pannyabot@tutanota.com>
2018-04-21 23:40:54 -0700

    ping

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var Discord = require('discord.js');
 var client = new Discord.Client();
-
+var request = require("request");
 
 client.on("message", function(message) {
     if (message.content === "ping") {
@@ -14,13 +14,14 @@ client.on("message", function(message) {
 //     }
 // });
 
-console.log("==========");
-console.log(process.env.NODE_ENV);
-
 if (process.env.NODE_ENV === "production") {
-    console.log(process.env.DISCORD_TOKEN)
     client.login(process.env.DISCORD_TOKEN);
 } else {
     var auth = require('./auth.json');
     client.login(auth.token);
 }
+
+setInterval(function () {
+    request('https://pannya-bot.herokuapp.com/', function (error, response, body) {
+    });
+}, 1000);
