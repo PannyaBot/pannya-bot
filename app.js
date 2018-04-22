1986c60f02a78499b4f36717adf1b26e5580d537
PannyaBot <pannyabot@tutanota.com>
2018-04-21 23:35:03 -0700

    dumb

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var Discord = require('discord.js');
 var client = new Discord.Client();
-var auth = require('./auth.json');
+
 
 client.on("message", function(message) {
     if (message.content === "ping") {
@@ -21,5 +21,6 @@ if (process.env.NODE_ENV === "production") {
     console.log(process.env.DISCORD_TOKEN)
     client.login(process.env.DISCORD_TOKEN);
 } else {
+    var auth = require('./auth.json');
     client.login(auth.token);
 }
